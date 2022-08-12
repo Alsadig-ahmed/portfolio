662c2932522c71e62a22d21e5722d8b4c50b75ec
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-12 20:04:53 +0200

    move project styles to its component

--- src/components/projects/Projects.svelte
+++ src/components/projects/Projects.svelte
@@ -1,53 +1,78 @@
-<section class="projects">
-    <h2>my project</h2>
-    <div class="flex">
-      <div>
-        <h3>wisdom project</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <img src="./images/IMG-20210511-WA0008.jpg" alt="" />
-      </div>
-      <div>
-        <h3>Lorem ipsum dolor sit.</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <img src="./images/IMG-20210511-WA0001.jpg" alt="" />
-      </div>
-      <div>
-        <h3>Lorem ipsum dolor sit amet.</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
-          ratione iste animi ad repudiandae itaque, soluta vitae recusandae,
-          iusto ex, hic magni error ipsam! Doloribus odit cumque corporis
-          aspernatur.
-        </p>
-        <img src="./images/IMG-20210401-WA0001.jpg" alt="" />
-      </div>
+<!-- <section class="projects">
+  <h2>my project</h2>
+  <div class="flex">
+    <div>
+      <h3>wisdom project</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <img src="./images/IMG-20210511-WA0008.jpg" alt="" />
     </div>
-  </section>
+    <div>
+      <h3>Lorem ipsum dolor sit.</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <img src="./images/IMG-20210511-WA0001.jpg" alt="" />
+    </div>
+    <div>
+      <h3>Lorem ipsum dolor sit amet.</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos
+        ratione iste animi ad repudiandae itaque, soluta vitae recusandae, iusto
+        ex, hic magni error ipsam! Doloribus odit cumque corporis aspernatur.
+      </p>
+      <img src="./images/IMG-20210401-WA0001.jpg" alt="" />
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  .projects {
+    border-radius: 0.7em;
+    padding: 0.3em 0.7em 0.7em 0.7em;
+    margin: 2em auto;
+    box-shadow: 0em 0.2em 0.9em 0.5em #29292954;
+    text-align: center;
+
+    @media (min-width: 700px) {
+      .flex {
+        display: flex;
+        div {
+          transition: transform 700ms 300ms;
+          &:hover {
+            transform: scale(1) translateY(-8%);
+          }
+        }
+      }
+    }
+    .flex div {
+      margin: 0.6em;
+      border-radius: 0.7em;
+      box-shadow: inset 0em 0.2em 0.9em 0.2em;
+    }
+    img {
+      border-radius: 9%;
+      width: 95%;
+    }
+  }
+</style> -->
