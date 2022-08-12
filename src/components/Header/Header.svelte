038c5b95a1b8c46ec59256b0f9adefd022ddd2c3
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-12 11:51:29 +0200

    move header styles to its component

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -12,3 +12,36 @@
       <img src="/DSC_0614.JPG" alt="Alsadig Ahmed" />
     </div>
   </header>
+  <style>
+    header {
+  padding: 0.3em 0.7em 0.7em 0.7em;
+  margin: 2em auto;
+  border-radius: 2em;
+  box-shadow: inset 0em 0.2em 0.9em 0.5em rgba(255, 255, 255, 0.514);
+}
+header h1 {
+  text-align: center;
+}
+@media (min-width: 600px) {
+  header div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+header div img {
+  border-radius: 8em;
+  transition: border-radius 800ms;
+  width: 16em;
+}
+@media (min-width: 600px) {
+  header div img {
+    border-radius: 11em;
+    width: 22em;
+  }
+}
+header div img:hover {
+  border-radius: 2em;
+}
+
+  </style>
